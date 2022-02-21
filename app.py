ed296a1061e4203cfe014909f3ab4a167a1a071e
Mario Gusman <mariogusman@gmail.com>
2022-02-21 14:38:20 +0000

    Screenshot function
    Takes a screenshot of the game's screen and save with timedate as name

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
+import PIL
+from PIL import ImageGrab  # to get the image from the screen
 from PIL import Image
+import pytesseract  # to ocr the image
+import time  # to create filename
+import win32gui  # to get the window handle of the game
 
-import pytesseract
 
 # If you don't have tesseract executable in your PATH, include the following:
 pytesseract.pytesseract.tesseract_cmd = r'D:\\Tesseract\\tesseract.exe'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+# Example tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
+
+
+def take_screenshot():
+    """
+    This function tries to focus the game by getting the window handle and setting it as foreground.
+    Returns warning if not possible.
+    This function takes a screenshot and saves it to a file in the screenshots folder.
+    """
+    try:
+        win32gui.SetForegroundWindow(win32gui.FindWindow(
+            None, 'LOST ARK (64-bit, DX11) v.2.0.1.2'))
+        print('Game focused.')
+        # sleeps for 1 second before continuing
+        time.sleep(1)
+        print('Taking screenshot...')
+
+        filename = f'screenshots\\{time.strftime("%Y-%m-%d_%H-%M-%S")}.png'
+        ImageGrab.grab().save(filename)
+        print(f'Screenshot saved to {filename}')
+    except:
+        print('Could not focus game.')
 
 
 def image_converter(image_path):
@@ -27,5 +52,4 @@ def numbers_only(text):
     return ''.join(filter(str.isdigit, text))
 
 
-print(numbers_only(image_converter(
-    'C:\\Users\\mario\\Documents\\GitHub\\lost-ark-queue-monitor\\screenshots\\teste1.jpg')))
+take_screenshot()
